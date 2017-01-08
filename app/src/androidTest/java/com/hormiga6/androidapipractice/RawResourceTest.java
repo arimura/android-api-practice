342e867215b79a441db7881d6fe67f639d58a162
Kotaro Arimura <kotaro_arimura@voyagegroup.com>
2017-01-08 12:31:03 +0900

    res/raw sample

---
+++ app/src/androidTest/java/com/hormiga6/androidapipractice/RawResourceTest.java
@@ -0,0 +1,31 @@
+package com.hormiga6.androidapipractice;
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.SmallTest;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(AndroidJUnit4.class)
+@SmallTest
+public class RawResourceTest {
+
+    @Test
+    public void testReadRaw() {
+        InputStream inputStream = InstrumentationRegistry.getTargetContext().getResources().openRawResource(R.raw.hoge);
+        try {
+            assertThat(IOUtils.readLines(inputStream, "UTF-8").get(0), is("hogehoge"));
+        } catch (IOException e) {
+        } finally {
+            IOUtils.closeQuietly(inputStream);
+        }
+    }
+}

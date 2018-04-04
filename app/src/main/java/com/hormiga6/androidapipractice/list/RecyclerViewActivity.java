ddc77f6c67f9eedb02919f05e9a811e1f1e307d1
Kotaro Arimura <kotaro_arimura@voyagegroup.com>
2018-04-04 19:55:02 +0900

    add recycler view scaffold

---
+++ app/src/main/java/com/hormiga6/androidapipractice/list/RecyclerViewActivity.java
@@ -0,0 +1,15 @@
+package com.hormiga6.androidapipractice.list;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hormiga6.androidapipractice.R;
+
+public class RecyclerViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view);
+    }
+}

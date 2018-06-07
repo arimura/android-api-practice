9eeeb3949bd1c8985b8113f3dac6c05e4c4ccba0
Kotaro Arimura <kotaro_arimura@voyagegroup.com>
2018-06-07 21:37:57 +0900

    audio player sample

--- app/src/main/java/com/hormiga6/androidapipractice/audio/AudioFocusActivity.java
+++ app/src/main/java/com/hormiga6/androidapipractice/audio/AudioFocusActivity.java
@@ -1,27 +1,103 @@
 package com.hormiga6.androidapipractice.audio;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
 import android.media.AudioAttributes;
 import android.media.AudioFocusRequest;
 import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
 
 import com.hormiga6.androidapipractice.R;
 
-public class AudioFocusActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.io.InputStream;
 
+public class AudioFocusActivity extends AppCompatActivity {
     private static final String TAG =  AudioFocusActivity.class.getSimpleName();
+    private static final int MEDIA_RES_ID = R.raw.sound;
+    private SeekBar mSeekBarAudio;
+    private PlayerAdapter mPlayerAdapter;
+    private boolean mUserIsSeeking = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audio_focus);
+        initializeUI();
+        initializeSeekbar();
+        initializePlaybackController();
+    }
 
-        requestAudoFocus();
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mPlayerAdapter.loadMedia(MEDIA_RES_ID);
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        if(isChangingConfigurations() && mPlayerAdapter.isPlaying()){
+            Log.d(TAG, "onStop: don't stop becasue it's rotating");
+        }else {
+            mPlayerAdapter.release();
+        }
+    }
+
+    private void initializeUI(){
+        Button mPlayButton = findViewById(R.id.button_play);
+        Button mPauseButton = findViewById(R.id.button_pause);
+        Button mResetButton = findViewById(R.id.button_reset);
+        mSeekBarAudio = findViewById(R.id.seekbar_audio);
+
+        mPauseButton.setOnClickListener(v -> {
+            mPlayerAdapter.pause();
+        });
+        mPlayButton.setOnClickListener(v -> {
+            mPlayerAdapter.play();
+        });
+        mResetButton.setOnClickListener(v -> {
+            mPlayerAdapter.reset();
+        });
+    }
+
+    private void initializePlaybackController(){
+        MediaPlayerHolder mediaPlayerHolder = new MediaPlayerHolder(this);
+        mediaPlayerHolder.setPlaybackInfoListener(new PlaybackListner());
+        mPlayerAdapter = mediaPlayerHolder;
+    }
+
+    private void initializeSeekbar(){
+        mSeekBarAudio.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int userSelectPosition = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(fromUser) {
+                    userSelectPosition = progress;
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                    mUserIsSeeking = false;
+                    mPlayerAdapter.seekTo(userSelectPosition);
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mUserIsSeeking = true;
+            }
+        });
     }
 
     //about audio focus https://medium.com/google-developers/audio-focus-1-6b32689e4380
@@ -57,6 +133,26 @@ public class AudioFocusActivity extends AppCompatActivity {
             case AudioManager.AUDIOFOCUS_REQUEST_GRANTED:
                 Log.d(TAG, "audio focus request granted");
                 break;
+        }
+    }
+
+    class PlaybackListner extends PlaybackInfoListener {
+        @Override
+        public void onDurationChanged(int duration){
+            mSeekBarAudio.setMax(duration);
+        }
+
+        @Override
+        public void onPositionChanged(int position){
+            if(!mUserIsSeeking){
+                mSeekBarAudio.setProgress(position);
+            }
+        }
+
+        @Override
+        public void onStateChanged(@State int state){
+            String stateToString = PlaybackInfoListener.convertStateToString(state);
+            Log.d(TAG, "onStateChanged: " + stateToString);
         }
     }
 }
---
+++ app/src/main/java/com/hormiga6/androidapipractice/audio/MediaPlayerHolder.java
@@ -0,0 +1,162 @@
+package com.hormiga6.androidapipractice.audio;
+
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class MediaPlayerHolder implements PlayerAdapter {
+    public static final int PLAYBACK_POSITION_REFREASH_INTERVAL_MS = 1000;
+    private final Context mContext;
+    private MediaPlayer mMediaPlayer;
+    private int mResourceId;
+    private PlaybackInfoListener mPlaybackInfoListener;
+    private ScheduledExecutorService mExecutor;
+    private Runnable mSeekbarPositionUpdateTask;
+
+    public MediaPlayerHolder(Context context) {
+        mContext = context.getApplicationContext();
+    }
+
+    private void initializeMediaPlayer(){
+        if(mMediaPlayer == null){
+            mMediaPlayer = new MediaPlayer();
+            mMediaPlayer.setOnCompletionListener(mp -> {
+                stopUpdatingCallbackWithPosition(true);
+                if(mPlaybackInfoListener != null){
+                    mPlaybackInfoListener.onStateChanged(PlaybackInfoListener.State.COMPLETED);
+                    mPlaybackInfoListener.onPlaybackCompleted();
+                }
+            });
+        }
+    }
+
+    public void setPlaybackInfoListener(PlaybackInfoListener listener){
+        mPlaybackInfoListener = listener;
+    }
+
+    private void stopUpdatingCallbackWithPosition(boolean resetUIPlaybackPosition){
+        if(mExecutor != null){
+            mExecutor.shutdownNow();
+            mExecutor = null;
+            mSeekbarPositionUpdateTask = null;
+            if(resetUIPlaybackPosition && mPlaybackInfoListener != null){
+                mPlaybackInfoListener.onDurationChanged(0);
+            }
+        }
+    }
+
+    private void startUpdatingCallbackWithPosition(){
+        if(mExecutor == null){
+            mExecutor = Executors.newSingleThreadScheduledExecutor();
+        }
+        if (mSeekbarPositionUpdateTask == null){
+            mSeekbarPositionUpdateTask = () -> {
+                updateProgressCallbavkTask();
+            };
+        }
+        mExecutor.scheduleAtFixedRate(mSeekbarPositionUpdateTask, 0, PLAYBACK_POSITION_REFREASH_INTERVAL_MS,
+                TimeUnit.MILLISECONDS);
+    }
+
+    private void updateProgressCallbavkTask(){
+        if(mMediaPlayer != null && mMediaPlayer.isPlaying()){
+            int currentPosition = mMediaPlayer.getCurrentPosition();
+            if(mPlaybackInfoListener != null){
+                mPlaybackInfoListener.onPositionChanged(currentPosition);
+            }
+        }
+    }
+
+    @Override
+    public void loadMedia(int resourceId) {
+        mResourceId = resourceId;
+        initializeMediaPlayer();
+
+        AssetFileDescriptor assetFileDescriptor = mContext.getResources().openRawResourceFd(
+                mResourceId);
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                mMediaPlayer.setDataSource(assetFileDescriptor);
+            }else{
+                mMediaPlayer.setDataSource(assetFileDescriptor.getFileDescriptor());
+            }
+            mMediaPlayer.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        initializeProgressCallback();
+    }
+
+    @Override
+    public void release() {
+        if(mMediaPlayer != null){
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+        }
+    }
+
+    @Override
+    public boolean isPlaying() {
+        if(mMediaPlayer != null){
+            return mMediaPlayer.isPlaying();
+        }
+        return false;
+    }
+
+    @Override
+    public void play() {
+        if(mMediaPlayer != null && !mMediaPlayer.isPlaying()){
+            mMediaPlayer.start();
+            if(mPlaybackInfoListener != null){
+                mPlaybackInfoListener.onStateChanged(PlaybackInfoListener.State.PLAYING);
+            }
+            startUpdatingCallbackWithPosition();
+        }
+
+    }
+
+    @Override
+    public void reset() {
+        if(mMediaPlayer != null){
+            mMediaPlayer.reset();
+            loadMedia(mResourceId);
+            if(mPlaybackInfoListener != null){
+                mPlaybackInfoListener.onStateChanged(PlaybackInfoListener.State.RESET);
+            }
+            stopUpdatingCallbackWithPosition(true);
+        }
+    }
+
+    @Override
+    public void pause() {
+        if(mMediaPlayer != null && mMediaPlayer.isPlaying()){
+            mMediaPlayer.pause();
+            if(mPlaybackInfoListener != null){
+                mPlaybackInfoListener.onStateChanged(PlaybackInfoListener.State.PAUSED);
+            }
+        }
+    }
+
+    @Override
+    public void initializeProgressCallback() {
+        final int duration = mMediaPlayer.getDuration();
+        if(mPlaybackInfoListener != null){
+            mPlaybackInfoListener.onDurationChanged(duration);
+            mPlaybackInfoListener.onPositionChanged(0);
+        }
+
+    }
+
+    @Override
+    public void seekTo(int position) {
+
+    }
+}

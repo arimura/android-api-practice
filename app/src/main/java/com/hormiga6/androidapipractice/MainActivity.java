4c000b07713f58d2b9f307a1276bf59439b578b3
Kotaro Arimura <kotaro_arimura@voyagegroup.com>
2018-07-23 14:33:53 +0900

    build constants sample

--- app/src/main/java/com/hormiga6/androidapipractice/MainActivity.java
+++ app/src/main/java/com/hormiga6/androidapipractice/MainActivity.java
@@ -31,6 +31,7 @@ import com.hormiga6.androidapipractice.Surface.GameActivity;
 import com.hormiga6.androidapipractice.UncaughtException.UncaughtExceptionActivity;
 import com.hormiga6.androidapipractice.audio.AudioFocusActivity;
 import com.hormiga6.androidapipractice.bitmap.BitmapActivity;
+import com.hormiga6.androidapipractice.build.BuildActivity;
 import com.hormiga6.androidapipractice.connectivity.ConnectivityActivity;
 import com.hormiga6.androidapipractice.localbroadcast.LocalBroadcastActivity;
 import com.hormiga6.androidapipractice.looper.MyLooperActivity;
@@ -60,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
         // specify an adapter (see also next example)
 
         List<DataSet> list = new ArrayList<DataSet>(){{
+            add(new DataSet("Build", BuildActivity.class));
             add(new DataSet("LocalBroadCaster", LocalBroadcastActivity.class));
             add(new DataSet("ConnectivityActivity", ConnectivityActivity.class));
             add(new DataSet("WindowManager", WindowActivity.class));
---
+++ app/src/main/java/com/hormiga6/androidapipractice/build/BuildActivity.java
@@ -0,0 +1,24 @@
+package com.hormiga6.androidapipractice.build;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.hormiga6.androidapipractice.R;
+
+public class BuildActivity extends AppCompatActivity {
+
+    private static final String TAG = BuildActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_build);
+
+        Log.d(TAG, "Build.DEVICE :" + Build.DEVICE);
+        Log.d(TAG, "Build.HARDWARE :" + Build.HARDWARE);
+        Log.d(TAG, "Build.MODEL :" + Build.MODEL);
+        Log.d(TAG, "Build.PRODUCT :" + Build.PRODUCT);
+    }
+}
